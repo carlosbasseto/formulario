e6a4e50954a4a1c62404c49d6dc78a9b447d4c4c
Dudu <127527178+carlosbasseto@users.noreply.github.com>
2024-06-19 13:46:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 function calculateScore() {
-    const questions = document.querySelectorAll('.question');
-    let totalScore = 0;
-
-    questions.forEach(question => {
-        const selectedAnswer = question.querySelector('input[type="radio"]:checked');
-        if (selectedAnswer) {
-            totalScore += parseInt(selectedAnswer.value);
+    var form = document.forms['quiz-form'];
+    var totalScore = 0;
+    for (var i = 1; i <= 10; i++) {
+        var question = form['question' + i];
+        for (var j = 0; j < question.length; j++) {
+            if (question[j].checked) {
+                totalScore += parseInt(question[j].value);
+            }
         }
-    });
-
-    document.getElementById('result').innerText = `Sua pontuação total é: ${totalScore}`;
+    }
+    var resultText = '';
+    if (totalScore <= 30) {
+        resultText = 'Sua saúde está horrível';
+    } else if (totalScore <= 50) {
+        resultText = 'Sua saúde está ruim';
+    } else if (totalScore <= 80) {
+        resultText = 'Sua saúde está boa';
+    } else if (totalScore < 101) {
+        resultText = 'Sua saúde está ótima';
+    } else {
+        resultText = 'Pontuação inválida';
+    }
+    document.getElementById('result').textContent = resultText;
 }
